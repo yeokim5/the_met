c21ead67d37e502b2c24268111627d4442216bd7
Dawn Kim <kymkhw279@gmail.com>
2023-08-23 00:29:30 +0900

    add like feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { department_data } from "./data/department.js";
 
+let global_search_data = "";
+const love_btn = document.querySelector(".love-btn");
 // 이것만 따로 js옮기기 파일 새로 random js
 // variable related to random
 const country = document.querySelector("#country");
@@ -14,6 +16,7 @@ let startHTML = `<option value="">Any Start Year</option>`;
 let endHTML = `<option value="">Any End Year</option>`;
 
 searchArtFetch(436203);
+load_like_art();
 
 department_data.departments.forEach((e) => {
   countryHTML += `<option value="${e.displayName}">${e.displayName}</option>`;
@@ -69,15 +72,11 @@ random_btn.addEventListener("click", () => {
 });
 
 async function getRandomFetch(string) {
-  console.log(
-    `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&${string}&q=""`
-  );
-
+  love_btn.classList.remove("active");
   const resp = await fetch(
     `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&${string}&q=""`
   );
   const respData = await resp.json();
-  console.log(respData);
 
   if (!respData || !respData.objectIDs || respData.objectIDs.length === 0) {
     // Handle the case when no data is returned or no object IDs are present
@@ -97,10 +96,10 @@ async function searchArtFetch(randomIndex) {
   );
 
   const searchData = await resp.json();
-
   if (searchData.message === "ObjectID not found") {
     getRandomFetch(random_string_api);
   }
+  global_search_data = searchData;
 
   console.log(searchData);
   let search = searchData.title + " " + searchData.artistAlphaSort;
@@ -128,4 +127,110 @@ async function searchArtFetch(randomIndex) {
           `;
 
   random_text.innerHTML = "";
+}
+
+love_btn.addEventListener("click", () => {
+  if (love_btn.classList.contains("active")) {
+    remvoeArtLS(global_search_data);
+    love_btn.classList.remove("active");
+  } else {
+    addArtLS(global_search_data);
+    love_btn.classList.add("active");
+  }
+  load_like_art();
+});
+
+// get art id from local storage
+function getArtLS() {
+  const artIds = JSON.parse(localStorage.getItem("artIds"));
+  return artIds === null ? [] : artIds;
+}
+
+// add art id to local storage
+function addArtLS(art) {
+  const artIds = getArtLS();
+  const artId = { title: art.title, image: art.primaryImage };
+  localStorage.setItem("artIds", JSON.stringify([...artIds, artId]));
+}
+
+// remove art id from local storage
+function remvoeArtLS(art) {
+  const artIds = getArtLS();
+
+  console.log(art.title);
+
+  localStorage.setItem(
+    "artIds",
+    JSON.stringify(artIds.filter((a) => a.title !== art.title))
+  );
+}
+
+// load like art
+function load_like_art() {
+  const liked_art_container = document.querySelector(".liked-art-container");
+  const art_array = JSON.parse(localStorage.getItem("artIds"));
+  liked_art_container.innerHTML = "";
+
+  art_array.forEach((element, index) => {
+    const artItem = document.createElement("li");
+    artItem.innerHTML = `
+      <img
+        src="${element.image}"
+        alt=""
+        class="popup-trigger"
+        data-index="${index}"
+      />
+      <br>
+      <p>${element.title}</p>
+    `;
+    liked_art_container.appendChild(artItem);
+  });
+
+  // Add click event to trigger popup
+  const popupTriggers = document.querySelectorAll(".popup-trigger");
+  popupTriggers.forEach((trigger) => {
+    trigger.addEventListener("click", (event) => {
+      const index = event.target.getAttribute("data-index");
+      openPopup(art_array[index]);
+    });
+  });
+}
+
+// Open the popup
+function openPopup(art) {
+  const modal = document.getElementById("artModal");
+  const modalImage = document.getElementById("modal-image");
+  const modalTitle = document.getElementById("modal-title");
+
+  modalImage.src = art.image;
+  modalTitle.textContent = art.title;
+
+  const existingDeleteButton = document.querySelector(".delete-button");
+  if (existingDeleteButton) {
+    existingDeleteButton.remove();
+  }
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete from Liked List";
+  deleteButton.classList.add("delete-button");
+  modalTitle.parentElement.appendChild(deleteButton);
+
+  modal.style.display = "block";
+
+  deleteButton.addEventListener("click", () => {
+    remvoeArtLS(art);
+    load_like_art();
+    modal.style.display = "none";
+  });
+
+  const closeBtn = document.querySelector(".close");
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 }
